95f54820c351b0839e4780e222758e4bd9ff36a5
LuisJhonatan <jhonatan.baca.5@gmail.com>
2025-02-26 01:31:12 -0500

    update image

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -58,27 +58,33 @@ export default function Home() {
           >
             <HomeSecondSection />
           </section> */}
-          <div className="w-full h-screen relative">
+          <div className="w-full min-h-screen relative">
             {imageLoaded && (
-              <img className="w-full h-full object-cover" src={fondo} alt="" />
+              <img className="w-full h-full object-cover  min-h-screen" src={fondo} alt="" />
             )}
             <div className="absolute inset-0 bg-black/15"></div>
 
-            <main className="w-full max-w-7xl text-white absolute top-[25vh] left-0 right-0 px-4 mx-auto z-40">
-              <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
-                Arquitectura interiores <b>AURA</b>
-              </h1>
-              <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-4">
-                <h3 className="relative md:left-[5%]">"Diseñamos</h3>
-                <h3 className="relative left-[15%]">espacios,</h3>
-                <h3 className="relative left-[25%]">creamos</h3>
-                <h3 className="relative left-[35%]">emociones"</h3>
+            <main className="w-full  text-white absolute top-[25vh] left-0 right-0   z-40">
+              <div className="bg-[var(--bg-primary-opacity)]">
+                <div className="max-w-7xl px-4 mx-auto">
+                  <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl  xl:text-6xl">
+                    Arquitectura interiores <b>AURA</b>
+                  </h1>
+                  <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl  py-4">
+                    <h3 className="relative md:left-[5%]">"Diseñamos</h3>
+                    <h3 className="relative left-[15%]">espacios,</h3>
+                    <h3 className="relative left-[25%]">creamos</h3>
+                    <h3 className="relative left-[35%]">emociones"</h3>
+                  </div>
+                </div>
               </div>
-              <p className="text-center text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl py-2">
-                En Aura, convertimos cada espacio en una experiencia única,
-                fusionando creatividad, funcionalidad y confort.
-              </p>
-              <div className="flex items-center justify-around lg:justify-baseline text-center py-4 md:text-xl">
+              <div className="max-w-7xl px-4 mx-auto">
+                <p className="text-center text-lg sm:text-xl md:text-2xl lg:text-3xl py-2">
+                  En Aura, convertimos cada espacio en una experiencia única,
+                  fusionando creatividad, funcionalidad y confort.
+                </p>
+              </div>
+              <div className="max-w-7xl mx-auto flex items-center justify-around lg:justify-baseline text-center py-4 md:text-xl">
                 <div className="flex text-[var(--bg-primary)] items-center justify-center w-[40%] lg:w-1/4 py-3 bg-[var(--bg-secondary)] rounded-xl">
                   <Link className="w-full h-full inline-block" to="/proyectos">
                     Ver proyectos {` >`}
@@ -94,7 +100,6 @@ export default function Home() {
                 </div>
               </div>
             </main>
-            <div className="w-full h-[25vh] absolute top-[25vh] bg-[var(--bg-primary-opacity)] z-0"></div>
           </div>
 
           <div
